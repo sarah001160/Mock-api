d60edafba70d13b7d5811d389f3abbe70c45e539
sarah <sarah001160@gmail.com>
2023-02-07 17:26:40 +0800

    correct typo middlewares

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server'); //get package from the json store
 const server = jsonServer.create(); //create and invoked like this及建立變數server
 const router = jsonServer.router('db.json');
-const middlwares = jsonServer.defaults();
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000; //shows what port do we serve to
 
 server.use(middlewares);  //所以我們才能在router使用middlewares
